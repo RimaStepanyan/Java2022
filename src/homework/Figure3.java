027c13373ffa7e024389b809414d570aa20c445d
RimaStepanyan <rima.stepanyan_93@mail.ru>
2022-05-13 03:33:05 +0400

    second example

---
+++ src/homework/Figure3.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class Figure3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j <= i; j++) {
+                System.out.print(" *");
+            }
+        }
+    }
+}
--- src/homework/FigurePainter2.java
+++ src/homework/FigurePainter2.java
@@ -10,15 +10,40 @@ public class FigurePainter2 {
 //           * * *
 //             * *
 //               *
+
+
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("  ");
+//            }
+//            for (int j = 4; j > i; j--) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+        // Figure2
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("  ");
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
             }
             for (int j = 4; j > i; j--) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+
 
     }
 }
